21e00c2cea12e8cc0f23ef0301aeef78a3f42b18
laposhko <laposhko0@gmail.com>
2025-01-18 18:09:22 +0200

    done without mobile bg

--- src/js/uniqueness.js
+++ src/js/uniqueness.js
@@ -2,12 +2,27 @@ document
   .querySelector('.uniqueness-list')
   .addEventListener('click', function () {
     const items = document.querySelectorAll(
-      '.uniqueness-list  .uniqueness-item'
+      '.uniqueness-list .uniqueness-item'
     );
+
     let currentIndex = [...items].findIndex(item =>
       item.classList.contains('visible-element')
     );
-    items[currentIndex].classList.remove('visible-element');
+
+    if (currentIndex === -1) return;
+
+    let currentItem = items[currentIndex];
     let nextIndex = (currentIndex + 1) % items.length;
-    items[nextIndex].classList.add('visible-element');
+    let nextItem = items[nextIndex];
+    let animationName = window.matchMedia('(min-width: 1024px)').matches
+      ? 'moveOut-desktop'
+      : 'moveOut-mobile';
+    currentItem.style.animation = `${animationName} 0.5s forwards`;
+    currentItem.addEventListener('animationend', function handleAnimationEnd() {
+      currentItem.classList.remove('visible-element');
+      currentItem.style.animation = ''; // Reset animation
+      nextItem.classList.add('visible-element');
+      nextItem.style.animation = 'fadeIn 0.5s forwards';
+      currentItem.removeEventListener('animationend', handleAnimationEnd);
+    });
   });
